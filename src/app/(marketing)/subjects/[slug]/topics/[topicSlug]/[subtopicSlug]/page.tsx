4d0202525f75d29f3831a325f81c6da0c65fbc1d
Ubuntu <ubuntu@ip-172-31-47-193.ap-southeast-1.compute.internal>
2026-05-16 15:31:08 +0000

    v15: 生物7空子主题+化学全部≥20MCQ

--- src/app/(marketing)/subjects/[slug]/topics/[topicSlug]/[subtopicSlug]/page.tsx
+++ src/app/(marketing)/subjects/[slug]/topics/[topicSlug]/[subtopicSlug]/page.tsx
@@ -1,4 +1,4 @@
-// force-redeploy-v14-fmt-names
+// force-redeploy-v15-fmt-names
 // DEBUG: subtopicId={typeof subtopicId} filterCol={filter?.col}
 import Link from "next/link";
 import { getSubtopic } from "@/lib/subtopic-data";
