d0d7836c71742639fc8a0c07d642dda7edccb97a
Ubuntu <ubuntu@ip-172-31-47-193.ap-southeast-1.compute.internal>
2026-05-24 07:21:29 +0000

    chore: remove temp key files

--- _dump_key.py
+++
@@ -1,7 +0,0 @@
-import re
-with open(".env.local") as f:
-    c = f.read()
-pk = re.search(r"ALIPAY_PRIVATE_KEY=(.+)", c).group(1).strip()
-with open("_alipay_key.txt", "w") as f:
-    f.write(pk)
-print("done, len:", len(pk))
